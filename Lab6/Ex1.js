858fa7aa2db57a2e2355577bedfe0f05c6c71f10
dport96 <dport@hawaii.edu>
2021-09-21 11:49:18 -1000

    xxx

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age = 55;
+var fav_num = 96;
+var day_of_birth = 14;
+var month_of_birth = 5;
+
+console.log(age + fav_num / (day_of_birth * month_of_birth) );
+
+// console.log( (age + fav_num) / day_of_birth * month_of_birth);
