91d5ecc1ea5fd6bdeee27b56bec1f5033bbc96cd
dport96 <dport@hawaii.edu>
2021-11-09 11:54:35 -1000

    xxx

--- Lab13/info_server_Ex2a.js
+++ Lab13/info_server_Ex2a.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
-app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/process_form', function (request, response, next) {
+
+    response.send('in POST /test ' + JSON.stringify(request.body) );
 });
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
+    next();
+});
+
+app.use(express.static('./public'));
+
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
