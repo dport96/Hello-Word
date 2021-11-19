d09dd3a4e50ffbe0aad4539963529370287396d5
dport96 <dport@hawaii.edu>
2021-11-19 12:35:00 -1000

    xxx

---
+++ Lab14/Ex1c.js
@@ -0,0 +1,9 @@
+//var userdata = require('./user_data.json');
+const fs = require('fs');
+
+
+var filename = 'user_data.json';
+var user_data_str = fs.readFileSync(filename, 'utf-8');
+var user_data_obj = JSON.parse(user_data_str);
+
+console.log(user_data_obj["kazman"]["password"]);
